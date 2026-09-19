e8b3caf5b78e47866c418a08b8974db97d8fd217
amirhs1003 <amirhs1003@atomicmail.io>
2026-09-19 23:15:16 +0330

    Change Telegram bot token
    Updated the Telegram bot token for security reasons.

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Cal
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-TELEGRAM_BOT_TOKEN = "8769828959:AAHC2CNYcIuf21qKKaRNAZ8GTiDaNuVuSjQ"
+TELEGRAM_BOT_TOKEN = "8769828959:AAFjM473ug72v8E3K82MhSUeLM6Iux4jf5s"
 OWNER_USERNAME = "amirhszz"
 owner_chat_id = 6422509900
 
